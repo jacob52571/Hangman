73da21c5811bb8dcae2184b69d158e4172868ce2
jacob52571 <lijacob290@gmail.com>
2024-02-01 22:12:33 +0000

    commented

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Hangman {
+    // all the good stuff
     private final String secretWord;
     private final char[] secretWordArray;
     private final List<Character> guessedLetters = new ArrayList<>();
@@ -13,7 +14,11 @@ public class Hangman {
     private char[] dashesArray;
     private final String category;
 
+    /**
+     * Constructor for the Hangman class.
+     */
     public Hangman() throws FileNotFoundException, IOException {
+        // adds all potential puzzles to the list
         FileReader reader = new FileReader("src/main/java/puzzles.txt");
         Scanner file = new Scanner(reader);
         List<String> words = new ArrayList<>();
@@ -22,6 +27,7 @@ public class Hangman {
             words.add(word);
         }
         file.close();
+        // removes all the used words from the list
         FileReader reader2 = new FileReader("src/main/java/used.txt");
         Scanner file2 = new Scanner(reader2);
         List<String> usedWords = new ArrayList<>();
@@ -33,15 +39,19 @@ public class Hangman {
         for (String word : usedWords) {
             words.remove(word);
         }
+        // if they used all the words, tell them
         if (words.size() == 0) {
             System.out.println("No more words!");
             System.exit(0);
         }
+        // picks a random word from the list and sets it as the secret word and the category
         String[] split = words.get((int) (Math.random() * words.size())).split(":");
         this.category = split[0];
         this.secretWord = split[1];
+        // puts the word into an array of characters
         this.secretWordArray = this.secretWord.toCharArray();
         this.dashesArray = new char[this.secretWordArray.length];
+        // fills the dashes array with dashes and / for spaces for the user to know what is left to guess
         for (int i = 0; i < this.secretWordArray.length; i++) {
             if (this.secretWordArray[i] == ' ') {
                 this.dashesArray[i] = '/';
@@ -51,18 +61,35 @@ public class Hangman {
         }
     }
 
+    /**
+     * Prints the list of dashes
+     * @return the dashes
+     */
     public String toString() {
         return new String(dashesArray);
     }
 
+    /**
+     * Gets the guess and updates the list of guessed characters
+     * @param c the guess
+     */
     public void getGuess(String c) {
+        // if it isn't a single letter, then tell them it's wrong
         if (c.length() != 1 || !c.matches("^[a-zA-Z]*$")) throw new IllegalArgumentException("Please enter a single letter.");
+        // if they already guessed that letter, tell them
         if (this.guessedLetters.contains(Character.toUpperCase(c.charAt(0)))) throw new RuntimeException("You already guessed that letter.");
+        // add the letter to the guessed letters
         this.guessedLetters.add(Character.toUpperCase(c.charAt(0)));
+        update();
     }
 
+    /**
+     * Updates the dashes array
+     */
     public void update() {
+        // creates a new array of characters
         char[] result = new char[this.secretWordArray.length];
+        // writes the letters or the dashes to the array
         for (int i = 0; i < this.secretWordArray.length; i++) {
             if (this.secretWordArray[i] == this.guessedLetters.get(this.guessedLetters.size() - 1)) {
                 result[i] = this.secretWordArray[i];
@@ -73,6 +100,7 @@ public class Hangman {
         this.dashesArray = result;
     }
 
+    // a bunch of getters and setters
     public void useGuess() { this.validGuesses--; }
 
     public int getValidGuesses() { return this.validGuesses; }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,26 @@ import java.io.*;
 
 // this code is messy but it works
 public class Main {
+    // throws the exceptions if the files aren't in the right places
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        // resets the used list of words to empty at the beginning
         FileWriter writer = new FileWriter("src/main/java/used.txt", false);
         writer.write("");
         writer.close();
+        // creates win/loss counter
         int wins = 0;
         int losses = 0;
+        // keep going until the user wants to stop
         while (true) {
+            // if they got the word, add one to win count
             if (play()) {
                 wins++;
-            } else {
+            }
+                // otherwise add one to the loss count
+            else {
                 losses++;
             }
+            // print total win/loss and ask of they want to play again
             System.out.println("Wins: " + wins + " Losses: " + losses);
             if (JOptionPane.showConfirmDialog(null, "Play again?") != 0) {
                 break;
@@ -22,37 +30,59 @@ public class Main {
         }
     }
 
+    /**
+     * Checks if a given list of characters has dashes. Makes checking if there's any unknown characters left.
+     * @param list the list of characters
+     * @return true if there are dashes, false if not
+     */
     public static boolean hasDashes(char[] dashes) {
         for (char c : dashes) {
+            // if at any point there is a '-', then return true
             if (c == '-') return true;
         }
         return false;
     }
 
+    /**
+     * Plays a game of hangman.
+     * @return true if the user won, false if they lost
+     * @throws FileNotFoundException if the file used.txt doesn't exist
+     * @throws IOException if the file used.txt is empty
+     */
     public static boolean play() throws FileNotFoundException, IOException {
+        // creates a new game
         Hangman hangman = new Hangman();
+        // prints the category
         System.out.println("The category is: " + hangman.getCategory());
+        // checks if they have any guesses left
         while (hangman.getValidGuesses() != 0) {
+            // gets the dashes
             char[] currentDashes = hangman.getDashes();
+            // if they already won, don't bother running the rest (just in case)
             if (hangman.getGuessed()) {
                 System.out.println("You win!");
                 break;
             }
+            // draw the hangman with the proper number of body parts based on number of guesses used
             drawHangman(hangman.getValidGuesses());
+            // print the dashes
             System.out.println(hangman);
+            // keep trying to get a valid guess
             try {
                 String guess = JOptionPane.showInputDialog("Guess a letter: ");
+                // if they cancel, stop asking them
                 if (guess == null) {
                     JOptionPane.showMessageDialog(null, "You quit the game.");
                     System.exit(0);
                 }
+                // put in the guess, the catch will print the error and ask again
                 hangman.getGuess(guess);
             }
             catch (RuntimeException e) {
                 System.out.println(e.getMessage());
                 continue;
             }
-            hangman.update();
+            // check if the new dashes (after guessing) are the same as before. this means their character was in the word and shouldn't count as a wrong guess
             char[] newDashes = hangman.getDashes();
             boolean same = true;
             for (int i = 0; i < currentDashes.length; i++) {
@@ -65,6 +95,7 @@ public class Main {
                 hangman.useGuess();
                 System.out.println("That letter is not in the word.");
             }
+            // if there aren't any more dashes, that means they won
             if (!hasDashes(newDashes)) {
                 hangman.setGuessed(true);
                 System.out.println("You win!");
@@ -73,9 +104,11 @@ public class Main {
             System.out.println("You have " + hangman.getValidGuesses() + " guesses left.");
         }
 
+        // writes the word to the used.txt file so that it won't be shown in the same session
         FileWriter writer = new FileWriter("src/main/java/used.txt", true);
         writer.write(hangman.getCategory() + ":" + hangman.getSecretWord() + "\n");
         writer.close();
+        // if they guessed the word, print the win message and vice versa
         if (hangman.getGuessed()) {
             System.out.println("You win! The word was " + hangman.getSecretWord());
             return true;
@@ -85,7 +118,12 @@ public class Main {
         }
     }
 
+    /**
+     * Draws the hangman based on the number of guesses left.
+     * @param guesses the number of guesses left
+     */
     public static void drawHangman(int guessesLeft) {
+        // does what it says it does
         System.out.println("  _______");
         if (guessesLeft < 7) {
             System.out.println("  |     |");
